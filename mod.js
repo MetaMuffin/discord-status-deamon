4f07c56426b1d690139adf147835f968837c4c15
cephox <40516991+cephox@users.noreply.github.com>
2020-09-30 18:22:03 +0200

    Added functionallity to only show own channel

--- mod.js
+++ mod.js
@@ -7,6 +7,7 @@ try {
         var self_bottom_bar = root.children[3].children[0].children[1].children[0].children[0].children[1].children[0].children[0].children[1].children[1].children[2]
         var self_muted = self_bottom_bar.children[0].getAttribute("aria-checked") == "true"
         var self_deaf = self_bottom_bar.children[1].getAttribute("aria-checked") == "true"
+        var self_name = root.children[3].children[0].children[1].children[0].children[0].children[1].children[0].children[0].children[1].children[1].children[1].children[0].children[0].textContent
         var users = []
         var no_user_info = false
     
@@ -21,36 +22,46 @@ try {
                 if (ch_e.children[1]) {
                     for (const user_e of ch_e.children[1].children) {
                         var user_c = user_e.children[0].children[0]
-                        var username = user_c.children[1].textContent
+                        var usr_name = user_c.children[1].textContent
                         
-                        var speaking = false
-                        user_c.children[0].classList.forEach((class_name) => {
-                            if (class_name.startsWith("avatarSpeaking")) speaking = true
-                        })
+                        if(usr_name == self_name) {
+
+                            for (const usr_e of ch_e.children[1].children) {
+                                var usr_c = usr_e.children[0].children[0]
+                                var username = usr_c.children[1].textContent
+
+                                var speaking = false
+                                usr_c.children[0].classList.forEach((class_name) => {
+                                    if (class_name.startsWith("avatarSpeaking")) speaking = true
+                                })
                         
-                        var icons_c = user_c.children[2]
-                        var mute_state = (icons_c) ? icons_c.children.length : 0
-                        var streaming = false
+                                var icons_c = usr_c.children[2]
+                                var mute_state = (icons_c) ? icons_c.children.length : 0
+                                var streaming = false
     
-                        if (icons_c) for (const e of icons_c.children) { 
-                            if (e.children[0].getAttribute("aria-label") == "Video") streaming = true
-                            if (e.children[0].textContent.startsWith("Live")) straming = true
+                                if (icons_c) for (const e of icons_c.children) { 
+                                    if (e.children[0].getAttribute("aria-label") == "Video") streaming = true
+                                    if (e.children[0].textContent.startsWith("Live")) straming = true
+                                }
+
+                                if (streaming) mute_state -= 1;
+
+                                var user_data = {
+                                    username,
+                                    speaking: speaking,
+                                    mute_state,
+                                    streaming,
+                                }
+
+                                user_data[0] = username
+
+                                users.push(user_data)
+                            }
                         }
-                        if (streaming) mute_state -= 1;
-                        
-                        var user_data = {
-                            username,
-                            speaking: speaking,
-                            mute_state,
-                            streaming,
-                        }
-                        users.push(user_data)
                     }
                 } 
             }
         }
-
-
 
         fetch("http://127.0.0.1:8123/update", {
             method: 'POST',
@@ -74,5 +85,3 @@ try {
     console.log("ERROROROOR");
     console.log(e);
 }
-
-
