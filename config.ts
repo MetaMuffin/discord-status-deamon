5635891f64c50e6bbfce19440bd476927e548df1
bgfxc4 <bindermarc@gmx.de>
2020-10-09 14:13:50 +0200

    added feature for nicknames

--- config.ts
+++ config.ts
@@ -1,24 +1,23 @@
 // Configuration for polybar color codes.
 export const config = {
-    
     colorReset: "%{F-}%{B-}%{U-}",
-    errorColor: "%{F#F00}%{B#000}",
+    errorColor: "%{F#F00}",
     modSeperator: "  || ",
-    
+
     showUsers: true,
-    onlyShowCurrentChannel: false, // This will only work if you dont change your nickname on the server. If somebody else has your name it will also show the users in his channel.
-    defaultColor: "%{F#409}",
-    deafColor: "%{B#666}%{F#FFF}",
-    mutedColor: "%{B#333}%{F#FFF}",
-    streamingColor: "%{U#33F}",
-    speakingColor: "%{B#000}%{F#0F0}",
+    onlyShowCurrentChannel: true, // This will only work if you dont change your nickname on the server. If somebody else has your name it will also show the users in his channel.
+    defaultColor: "%{F#FFF}",
+    deafColor: "%{F#FFF}",
+    mutedColor: "%{F#F00}",
+    streamingColor: "%{}",
+    speakingColor: "%{F#0F0}",
     channelSeperator: " || ",
     userSeperator: "| ",
-    
-    showFlags: true,
-    flagColor: "%{B#333}%{F#FFF}",
+
+    showFlags: false,
+    flagColor: "%{B#333}%{F#F00}",
 
     showSelfStatus: true,
-    selfStatusLabel: "%{U#409}ME: ",
-
-}
+    nickname: "Marc",
+    selfStatusLabel: "ME: ",
+};
--- index.ts
+++ index.ts
@@ -1,72 +1,80 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { config } from "./config";
 
-import express from "express"
-import bodyParser from "body-parser"
-import cors from "cors"
-import { config } from "./config"
-
-const app = express()
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
 
 export interface State {
-    channels: undefined | Array<Array<{
-        mute_state: number,
-        username: string,
-        speaking: boolean,
-        streaming: boolean,
-    }>>,
-    self_deaf: boolean,
-    self_muted: boolean,
-    self_name: string,
-    no_user_info: boolean,
+    channels:
+        | undefined
+        | Array<
+              Array<{
+                  mute_state: number;
+                  username: string;
+                  speaking: boolean;
+                  streaming: boolean;
+              }>
+          >;
+    self_deaf: boolean;
+    self_muted: boolean;
+    self_name: string;
+    no_user_info: boolean;
 }
 
-var state:State = {channels:undefined,self_deaf: false, self_muted: false, no_user_info: true,self_name: ""}
+var state: State = { channels: undefined, self_deaf: false, self_muted: false, no_user_info: true, self_name: "" };
 
-app.get("/status",(req,res) => {
-    var bar = ""
-    if (!state.channels) return res.send("Cant get info")
-    
+app.get("/status", (req, res) => {
+    var bar = "";
+    if (!state.channels) return res.send("Cant get info");
+
     if (config.showSelfStatus) {
-        bar += config.selfStatusLabel + config.colorReset
-        bar += state.self_muted ? `${config.flagColor}M${config.colorReset}` : " "
-        bar += state.self_deaf ? `${config.flagColor}D${config.colorReset}` : " "
-        bar += config.colorReset + config.modSeperator + config.colorReset
+        bar += config.selfStatusLabel + config.colorReset;
+        bar += state.self_muted ? `${config.flagColor}M${config.colorReset}` : " ";
+        bar += state.self_deaf ? `${config.flagColor}D${config.colorReset}` : " ";
+        bar += config.colorReset + config.modSeperator + config.colorReset;
     }
 
     if (config.showUsers) {
-        if (state.no_user_info) bar += config.errorColor + "No user info" + config.colorReset
-        var chs_filtered = state.channels.filter(ch => (ch.length > 0))
+        if (state.no_user_info) bar += config.errorColor + "No user info" + config.colorReset;
+        var chs_filtered = state.channels.filter((ch) => ch.length > 0);
 
-        bar += chs_filtered.map(ch => {
-            if (!ch.find(u => (u.username == state.self_name)) && config.onlyShowCurrentChannel) return "";
-            return ch.map(u => {
-                var color = config.defaultColor;
-                if (u.speaking) color = config.speakingColor; 
-                if (u.mute_state > 0) color = config.mutedColor;
-                if (u.mute_state > 1) color = config.deafColor;
-                if (u.streaming) color += config.streamingColor;
-                
-                var flags = config.showFlags ? (" "
-                    + (u.speaking ? `${config.flagColor}S${config.colorReset}` : " ")
-                    + ((u.mute_state > 0) ? `${config.flagColor}M${config.colorReset}` : " ")
-                    + ((u.mute_state > 1) ? `${config.flagColor}D${config.colorReset}` : " ")
-                    + ((u.streaming) ? `${config.flagColor}V${config.colorReset}` : " ")
-                ) : ""
-                return `${color}${u.username}${config.colorReset}${flags}`
-            }).join(config.userSeperator)
-        }).join(config.onlyShowCurrentChannel ? "" : config.channelSeperator)
+        bar += chs_filtered
+            .map((ch) => {
+                if (!ch.find((u) => (u.username == state.self_name ||u.username == config.nickname)) && config.onlyShowCurrentChannel) return "";
+                return ch
+                    .map((u) => {
+                        var color = config.defaultColor;
+                        if (u.speaking) color = config.speakingColor;
+                        if (u.mute_state > 0) color = config.mutedColor;
+                        if (u.mute_state > 1) color = config.deafColor;
+                        if (u.streaming) color += config.streamingColor;
+
+                        var flags = config.showFlags
+                            ? " " +
+                              (u.speaking ? `${config.flagColor}S${config.colorReset}` : " ") +
+                              (u.mute_state > 0 ? `${config.flagColor}M${config.colorReset}` : " ") +
+                              (u.mute_state > 1 ? `${config.flagColor}D${config.colorReset}` : " ") +
+                              (u.streaming ? `${config.flagColor}V${config.colorReset}` : " ")
+                            : "";
+                        return `${color}${u.username}${config.colorReset}${flags}`;
+                    })
+                    .join(config.userSeperator);
+            })
+            .join(config.onlyShowCurrentChannel ? "" : config.channelSeperator);
     }
-    
-    res.send(bar)
-})
 
-app.options("/update", cors())
-app.post("/update", cors(), (req,res) => {
+    res.send(bar);
+});
+
+app.options("/update", cors());
+app.post("/update", cors(), (req, res) => {
     state = req.body;
-    res.send("OK")
-})
+    res.send("OK");
+});
 
-
-app.listen(8123,"127.0.0.1",() => {
+app.listen(8123, "127.0.0.1", () => {
     console.log("Server running!");
-})
+});
+
